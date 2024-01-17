7ef23873eaa13989e1028c73352a7b2ee3cf4e53
isabel.najarro <isabel.najarro@accenture.com>
2024-01-17 12:00:02 +0100

    manipulacion archivos

---
+++ manipulacionArchivos.py
@@ -0,0 +1,39 @@
+fichero = "mi_archivo.txt"
+
+try:
+    with open(fichero, "r") as archivo:
+        contenido = archivo.read()
+        print(contenido)
+except FileNotFoundError:
+    print("El archivo no se encontró. Creando el archivo...")
+    with open(fichero, "w") as archivo:
+        archivo.write("ABCDEFGHIKLMNO\n")
+        archivo.write("1234567890\n")
+        archivo.write("+-*/\n")
+        archivo.write(".,:;-_\n")
+        archivo.write("PQRSTUVWXYZ\n")
+    print("Archivo creado")
+
+with open(fichero, 'r', encoding='UTF8') as archivo:
+    linea = archivo.readline()
+    posicion_inicial = archivo.seek(0)
+    while linea:
+        print(linea.strip())
+        pos = archivo.tell()
+        print("Posición del cursor: ", pos)
+        linea = archivo.readline()
+
+with open(fichero, "a+") as archivo_escritura:
+    archivo_escritura.write("AaEeIiOoUu")
+
+with open(fichero, "a+") as archivo_anexar:
+    archivo_anexar.seek(0)
+    contenido = archivo_anexar.read()
+    print("\nContenido completo después de anexar:", contenido)
+
+try:
+    with open(fichero, "a") as archivo_error:
+        pass
+except Exception as e:
+    print("\nError al abrir en modo 'a':", e)
+    print("Explicación: 'a' solo permite escritura al final del archivo, no lectura.")
