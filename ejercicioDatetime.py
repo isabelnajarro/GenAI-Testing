04d899dc93da41368bfef8b9973044fb050f4487
isabel.najarro <isabel.najarro@accenture.com>
2024-01-17 09:00:00 +0100

    datetime exercise

--- ejercicioDatetime.py
+++ ejercicioDatetime.py
@@ -9,8 +9,19 @@ print(nacimiento.day)
 print(nacimiento.month)
 print(nacimiento.year)
 
+def obtener_dia(num):
+    switch = {
+        0: "Lunes",
+        1: "Martes",
+        2: "Miércoles",
+        3: "Jueves",
+        4: "Viernes",
+        5: "Sábado",
+        6: "Domingo"
+    }
+
 print("Dia de la semana que naci, teniendo en cuenta que se empieza en 0: ", nacimiento.weekday())
-nombre_dia = nacimiento.strftime('%A')
+nombre_dia = obtener_dia(nacimiento.weekday())
 print("Naci un: ", nombre_dia)
 
 diferencia = datetime.now() - nacimiento
